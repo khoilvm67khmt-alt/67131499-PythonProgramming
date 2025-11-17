b47d69a2354ad7951b50809ae619a9e351e887dd
khoilvm67khmt-alt <khoi.lvm.67khmt@ntu.edu.vn>
2025-11-17 08:12:23 +0700

    Create hoan_doi_gia_tri.py

---
+++ hoan_doi_gia_tri.py
@@ -0,0 +1,8 @@
+print('Hoán đổi giá trị')
+A=float(input('Hãy nhập số A = '))
+B=float(input('Hãy nhập số B = '))
+A , B = B , A
+print("Kết quả sau khi bị hoán đổi")
+print('A = ', A)
+print('B = ', B)
+        
