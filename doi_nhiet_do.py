abeb38f2aa2aeec6f50beebdc8747bc7d9dc2dea
khoilvm67khmt-alt <khoi.lvm.67khmt@ntu.edu.vn>
2025-11-17 08:28:52 +0700

    file bài tập

--- doi_nhiet_do.py
+++
@@ -1,4 +0,0 @@
-print('đây là chương trình chuyển đổi nhiệt độ Celsius sang Fahrenhiet')
-C=float(input('Hãy nhập nhiệt độ (°C) = '))
-print('Nhiệt độ sau khi đổi là :', (C*9/5)+32)
-        
--- hoan_doi_gia_tri.py
+++
@@ -1,8 +0,0 @@
-print('Hoán đổi giá trị')
-A=float(input('Hãy nhập số A = '))
-B=float(input('Hãy nhập số B = '))
-A , B = B , A
-print("Kết quả sau khi bị hoán đổi")
-print('A = ', A)
-print('B = ', B)
-        
