5c3eb8892828d04a5a31c2defd185e3ac8703335
khoilvm67khmt-alt <khoi.lvm.67khmt@ntu.edu.vn>
2025-11-17 08:20:16 +0700

    Create tinh_thoi_gian.py

---
+++ tinh_thoi_gian.py
@@ -0,0 +1,7 @@
+print('Đây là chương trình tính thời gian')
+H=float(input('Hãy nhập số giờ cần tính '))
+S=float(input('Hãy nhập số phút cần tính '))
+P=float(input('Hãy nhập số giây cần tính '))
+A=H*3600
+B=P*60
+print('tổng số giây là : ', A+B+S)
