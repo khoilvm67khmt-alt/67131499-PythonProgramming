70635390802e5b58fbe1cb3fda9595e4891a4398
khoilvm67khmt-alt <khoi.lvm.67khmt@ntu.edu.vn>
2025-11-17 08:24:51 +0700

    Create lai_suat_don.py

---
+++ lai_suat_don.py
@@ -0,0 +1,6 @@
+print('Đây là chương trình tính lãi suất đơn')
+A=float(input('Số tiền gửi : '))
+B=float(input('lãi suất (%/năm) : '))
+C=int(input('Số tháng gửi : '))
+print('Tiền lãi của bạn là : ', (A*B*C)/12)
+        
