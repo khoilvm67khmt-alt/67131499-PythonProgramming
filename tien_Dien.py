5bf9e25262a5effc97fb14f002e43b54bbcc1e7c
khoilvm67khmt-alt <khoi.lvm.67khmt@ntu.edu.vn>
2025-11-17 08:01:07 +0700

    Create tien_Dien.py

---
+++ tien_Dien.py
@@ -0,0 +1,5 @@
+print('Đây là chương trình tính tiền điện')
+A= 2.000 # VND/kWh.
+B=float(input('Hãy nhập số kWh điện tiêu thụ của bạn '))
+print(' Tiền điện của bạn là : ', A*B , 'VND/kWh')
+        
