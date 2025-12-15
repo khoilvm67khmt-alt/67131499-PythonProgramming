93ba6786f81005ca88a71b960e682361fe091cc2
khoilvm67khmt-alt <khoi.lvm.67khmt@ntu.edu.vn>
2025-12-15 14:04:38 +0700

    Create TH15_Bai2_IF.py

---
+++ cac_bai_on_them_python/bai_tap_ngay_15_thang_12_2025/TH15_Bai2_IF.py
@@ -0,0 +1,6 @@
+print("xin chào đây là chương trình nhật biết số có chia hết cho 3 hay không")
+A=float(input("hãy nhập số của bạn: "))
+if A % 3 == 0 :
+    print("số này chia hết cho 3")
+else :
+    print("số này không chia hết cho 3")
