77185dfecef8801755b475f2cd001b22229845ce
khoilvm67khmt-alt <khoi.lvm.67khmt@ntu.edu.vn>
2025-12-15 13:57:57 +0700

    Create TH15_Bai1_Square.py

---
+++ cac_bai_on_them_python/bai_tap_ngay_15_thang_12_2025/TH15_Bai1_Square.py
@@ -0,0 +1,8 @@
+print("Đây là chương trình tính chu vì và diện tích hình chữ nhật")
+A=float(input("hãy nhập chiều dài: "))
+B=float(input("hãy nhập chiều rộng: "))
+dien_tich= A*B
+chu_vi= 2*(A+B)
+print("Diện tích của hình chữ nhật là :", dien_tich)
+print("Chu vi của hình chữ nhật là :", chu_vi)
+
