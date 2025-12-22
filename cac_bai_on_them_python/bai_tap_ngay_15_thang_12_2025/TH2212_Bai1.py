c461f4b9defbb69a7778756044873d8f7c1227a1
khoilvm67khmt-alt <khoi.lvm.67khmt@ntu.edu.vn>
2025-12-22 14:39:53 +0700

    Update TH2212_Bai1.py

--- cac_bai_on_them_python/bai_tap_ngay_15_thang_12_2025/TH2212_Bai1.py
+++ cac_bai_on_them_python/bai_tap_ngay_15_thang_12_2025/TH2212_Bai1.py
@@ -1,6 +1,11 @@
-w=float(input("Nhập chiều rộng(rộng<=0.0): "))
-h=float(input("Nhập chiều dài(cao<=100.0): "))
-chu_vi= 2*(w+h)
-dien_tich= (w*h)
-print(f"Chu vi{chu_vi :.2f}")
-print(f"Diện tích{dien_tich :.2f}")
+while True:
+    w = float(input("Nhập chiều rộng (w >= 0): "))
+    h = float(input("Nhập chiều dài (h <= 100): "))
+    if w >= 0 and h <= 100:
+        break
+    else:
+        print("Sai, nhập lại!")
+chu_vi = 2 * (w + h)
+dien_tich = w * h
+print(f"Chu vi: {chu_vi:.2f}")
+print(f"Diện tích: {dien_tich:.2f}")
